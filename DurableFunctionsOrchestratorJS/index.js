b7ecc7e1ae5352b82b28c4e7de80ed610a5146c0
sea-asteroid <gongqingyuan@fudan.edu.cn>
2019-11-15 11:46:07 +0100

    Concurrent running experiments-Orchestrator

---
+++ DurableFunctionsOrchestratorJS/index.js
@@ -0,0 +1,75 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require("durable-functions");
+var count = 0;
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+    /*
+    let instanceID = context.df.instanceId;
+    console.log('InstanceID', instanceID, 'Start count = ', count);
+
+    count += 1;
+
+    let tmp1 = yield context.df.callActivity("Hello", count instanceID);
+    console.log(instanceID, ' one count = %d', tmp1);
+    outputs.push(tmp1);
+
+    let tmp2 = yield context.df.callActivity("Hello", tmp1, instanceID);
+    console.log(instanceID, ' two count = %d', tmp2);
+    outputs.push(tmp2);
+
+    let tmp3 = yield context.df.callActivity("Hello", tmp2, instanceID);
+    console.log(instanceID, ' three count = %d', tmp3);
+    outputs.push(tmp3);
+
+    return outputs;
+    */
+   /*
+   json encoding/decoding from Paarajat
+   var count = 0;
+   let instanceID = context.df.instanceId;
+   var obj = {'id': instanceID, 'count': count};
+   console.log('InstanceID', instanceID, 'Start count = ', count);
+   count += 1;
+   let tmp1 = yield context.df.callActivity("Hello", JSON.stringify(obj));
+   console.log(instanceID + ' one count = %d', tmp1);
+   outputs.push(tmp1);
+   var obj = {'id': instanceID, 'count': tmp1};
+   let tmp2 = yield context.df.callActivity("Hello", JSON.stringify(obj));
+   console.log(instanceID + ' two count = %d', tmp2);
+   outputs.push(tmp2);
+   var obj = {'id': instanceID, 'count': tmp2};
+   let tmp3 = yield context.df.callActivity("Hello", JSON.stringify(obj));
+   console.log(instanceID + ' three count = %d', tmp3);
+   outputs.push(tmp3);
+   return outputs;
+   */
+  let instanceID = context.df.instanceId;
+  console.log('InstanceID' + instanceID, 'Start count = ', count);
+
+  count += 1;
+
+  let tmp1 = yield context.df.callActivity("Hello", `${count} ${instanceID}`);
+  console.log(instanceID + ' one count = %d', tmp1);
+  outputs.push(tmp1);
+
+  let tmp2 = yield context.df.callActivity("Hello", `${tmp1} ${instanceID}`);
+  console.log(instanceID + ' two count = %d', tmp2);
+  outputs.push(tmp2);
+
+  let tmp3 = yield context.df.callActivity("Hello", `${tmp2} ${instanceID}`);
+  console.log(instanceID + ' three count = %d', tmp3);
+  outputs.push(tmp3);
+
+  return outputs;
+
+});
---
+++ Hello/index.js
@@ -0,0 +1,68 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+module.exports = async function (context) {
+    //return `Hello ${context.bindings.name}!`;
+    //let instanceID = context.bindings.instanceID;
+    var num = 1000000000;
+    function fibonacci(num){
+        var a = 1, b = 0, temp;
+        while (num >= 0){
+          temp = a;
+          a = a + b;
+          b = temp;
+          num--;
+        } 
+        return b;
+    }
+    console.log(context.bindings.name);
+    var res = context.bindings.name;
+    console.log('res', res);
+    var obj = res.split(" ");
+    obj_id = obj[1];
+    obj_count = obj[0];
+    console.log('obj_id', obj_id);
+    console.log('obj_count', obj_count);
+    
+    let time_start_fibonacci = new Date().getTime();
+
+    console.log(obj_id + ' time_start_fibonacci =', time_start_fibonacci);
+    let va = fibonacci(num);
+    let time_end_fibonacci = new Date().getTime();
+  
+    console.log(obj_id + ' time_end_fibonacci =', time_end_fibonacci);
+    time_wait = time_end_fibonacci - time_start_fibonacci;
+  
+    //obj.count
+    console.log(obj_id + ' time_duratioin_fibonacci =', time_wait);
+    var aaa = parseInt(obj_count, 10);
+    return aaa+100;
+
+    /*
+    console.log(context.bindings.name)
+    var obj = JSON.parse(context.bindings.name);
+    let time_start_fibonacci = new Date().getTime();
+    //let time_start_fibonacci = context.df.getTime();
+    console.log(obj.id + ' time_start_fibonacci =', time_start_fibonacci);
+    let va = fibonacci(num);
+    let time_end_fibonacci = new Date().getTime();
+    //let time_end_fibonacci = context.df.getTime();
+    console.log(obj.id + ' time_end_fibonacci =', time_end_fibonacci);
+    time_wait = time_end_fibonacci - time_start_fibonacci;
+    
+    
+    //obj.count
+    console.log(obj.id + ' time_duratioin_fibonacci =', time_wait);
+    //return context.bindings.name; 
+    return obj.count+100;
+    */
+
+};
