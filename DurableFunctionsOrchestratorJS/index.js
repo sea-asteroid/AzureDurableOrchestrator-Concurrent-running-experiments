065400eafdc66e865182a03a916a5302d0c2f3a3
sea-asteroid <gongqingyuan@fudan.edu.cn>
2019-11-26 13:42:30 +0100

    measurement

--- DurableFunctionsOrchestratorJS/index.js
+++ DurableFunctionsOrchestratorJS/index.js
@@ -10,66 +10,56 @@
  */
 
 const df = require("durable-functions");
-var count = 0;
 module.exports = df.orchestrator(function* (context) {
-    const outputs = [];
-    /*
-    let instanceID = context.df.instanceId;
-    console.log('InstanceID', instanceID, 'Start count = ', count);
-
+  const outputs = [];
+  var count = 0;
+  var tmp1 = 0;
+  var tmp2 = 0;
+  var tmp3 = 0;
+  var str = "";
+  var i = 0;
+  var num =0;
+  var instanceID = 0;
+  for (num = 3; num > 1; num--){
+    str = "a";
+    for (i = 2048*num; i > 0; i --) {
+      str += "String concatenation. ";
+    };
+    instanceID = context.df.instanceId;
+    console.log('InstanceID = ' + instanceID + ' Start count = ', count);
     count += 1;
 
-    let tmp1 = yield context.df.callActivity("Hello", count instanceID);
-    console.log(instanceID, ' one count = %d', tmp1);
+    //console.log('InstanceID = ' + instanceID + ' time_start_01_pass =' + new Date().getTime());
+    time_start_01_pass = new Date().getTime();
+    tmp1 = yield context.df.callActivity("Hello", `${count} ${instanceID} ${str}`);
+    //console.log('InstanceID = ' + instanceID + ' time_end_01_pass =' + new Date().getTime());
+    time_end_01_pass = new Date().getTime(); 
+    time_duration_01_pass = time_end_01_pass - time_start_01_pass;
+    console.log('InstanceID = ' + instanceID + ' time_duration_01_pass =' + time_duration_01_pass);
+    //context.bindings.myInputBlog = 'InstanceID = ' + instanceID + ' time_duration_01_pass =' + time_duration_01_pass;
+    //context.bindings.myOutputBlob = context.bindings.myInputBlob;
+    //context.done();
     outputs.push(tmp1);
-
-    let tmp2 = yield context.df.callActivity("Hello", tmp1, instanceID);
-    console.log(instanceID, ' two count = %d', tmp2);
+    
+    //console.log('InstanceID = ' + instanceID + ' time_start_12_pass =' + new Date().getTime());
+    time_start_12_pass = new Date().getTime();
+    tmp2 = yield context.df.callActivity("Hello", `${tmp1} ${instanceID} ${str}`);
+    //console.log('InstanceID = ' + instanceID + ' time_end_12_pass =' + new Date().getTime());
+    time_end_12_pass = new Date().getTime(); 
+    time_duration_12_pass = time_end_12_pass - time_start_12_pass;
+    console.log('InstanceID = ' + instanceID + ' time_duration_12_pass =' + time_duration_12_pass);
     outputs.push(tmp2);
+    context.bindings.
 
-    let tmp3 = yield context.df.callActivity("Hello", tmp2, instanceID);
-    console.log(instanceID, ' three count = %d', tmp3);
+    //console.log('InstanceID = ' + instanceID + ' time_start_23_pass =' + new Date().getTime());
+    time_start_23_pass = new Date().getTime();
+    tmp3 = yield context.df.callActivity("Hello", `${tmp2} ${instanceID} ${str}`);
+    //console.log('InstanceID = ' + instanceID + ' time_end_23_pass =' + new Date().getTime());
+    time_end_23_pass = new Date().getTime(); 
+    time_duration_23_pass = time_end_23_pass - time_start_23_pass;
+    
     outputs.push(tmp3);
-
-    return outputs;
-    */
-   /*
-   json encoding/decoding from Paarajat
-   var count = 0;
-   let instanceID = context.df.instanceId;
-   var obj = {'id': instanceID, 'count': count};
-   console.log('InstanceID', instanceID, 'Start count = ', count);
-   count += 1;
-   let tmp1 = yield context.df.callActivity("Hello", JSON.stringify(obj));
-   console.log(instanceID + ' one count = %d', tmp1);
-   outputs.push(tmp1);
-   var obj = {'id': instanceID, 'count': tmp1};
-   let tmp2 = yield context.df.callActivity("Hello", JSON.stringify(obj));
-   console.log(instanceID + ' two count = %d', tmp2);
-   outputs.push(tmp2);
-   var obj = {'id': instanceID, 'count': tmp2};
-   let tmp3 = yield context.df.callActivity("Hello", JSON.stringify(obj));
-   console.log(instanceID + ' three count = %d', tmp3);
-   outputs.push(tmp3);
-   return outputs;
-   */
-  let instanceID = context.df.instanceId;
-  console.log('InstanceID = ' + instanceID, 'Start count = ', count);
-
-  count += 1;
-
-  let tmp1 = yield context.df.callActivity("Hello", `${count} ${instanceID}`);
-  console.log('InstanceID = ' + instanceID + ' one count = %d', tmp1);
-  outputs.push(tmp1);
-
-  let tmp2 = yield context.df.callActivity("Hello", `${tmp1} ${instanceID}`);
-  console.log('InstanceID = ' + instanceID + ' two count = %d', tmp2);
-  outputs.push(tmp2);
-
-  let tmp3 = yield context.df.callActivity("Hello", `${tmp2} ${instanceID}`);
-  console.log('InstanceID = ' + instanceID + ' three count = %d', tmp3);
-  outputs.push(tmp3);
-
+  }
   return outputs;
-
 });
+
